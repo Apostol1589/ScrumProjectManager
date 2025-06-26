c14c1519b6e3e86af56f9addc5ddb83ba949b5be
Apostol <jeburek14oo@gmail.com>
2025-06-26 22:53:28 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ScrumProjectManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
